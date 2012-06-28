5bcbbb78061acfaf538cbe7bf7f836e981e3ca4d
Larry Franks <larry.franks@microsoft.com>
2012-06-28 17:33:00 -0400

    adding dalli gem to demonstrate accessing cache via memcache protocol

--- WorkerRole/app/app.rb
+++ WorkerRole/app/app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
+require 'dalli'
 
 set :server, :thin
 set :port, ENV['PORT']
 set :bind, ENV['ADDRESS']
 
+dc = Dalli::Client.new('localhost_WorkerRole:11211')
+dc.set('message', 'Hello World!')
+
 get '/' do
-	"Hello World!"
+	dc.get('message')
 end
