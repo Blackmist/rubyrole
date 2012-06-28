e48be64ce4f0734ee1be31c3855e86a534aac67e
Larry Franks <larry.franks@microsoft.com>
2012-06-28 17:59:47 -0400

    changing connection string

--- WorkerRole/app/app.rb
+++ WorkerRole/app/app.rb
@@ -5,7 +5,7 @@ set :server, :thin
 set :port, ENV['PORT']
 set :bind, ENV['ADDRESS']
 
-dc = Dalli::Client.new('localhost_WorkerRole:11211')
+dc = Dalli::Client.new('localhost:11211')
 dc.set('message', 'Hello World!')
 
 get '/' do
